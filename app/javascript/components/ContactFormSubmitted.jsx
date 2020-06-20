993f94d7766daaefa0457527251a73ebd5b3533d
Steve Torrens <stevetorrens70@gmail.com>
2020-06-20 21:14:35 +1200

    Commented out more of contact

--- app/javascript/components/ContactFormSubmitted.jsx
+++ app/javascript/components/ContactFormSubmitted.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { showContactForm } from '../actions/portfolio'
-import { Link } from 'react-scroll'
+// import React from 'react'
+// import { connect } from 'react-redux'
+// import { showContactForm } from '../actions/portfolio'
+// import { Link } from 'react-scroll'
 
-const ContactFormSubmitted = (props) => (
-  <React.Fragment>
-    <div className="content is-size-6 has-text-centered">
-      <p>Thanks for your message</p>
-      <Link activeClass="active" className="Contact" to="Contact" spy={true} smooth={true} duration={200} >
-        <button className="button is-white has-text-link" onClick={()=> props.dispatch(showContactForm())}>Return to contact form</button>
-      </Link>
-    </div>
-  </React.Fragment>
-)
+// const ContactFormSubmitted = (props) => (
+//   <React.Fragment>
+//     <div className="content is-size-6 has-text-centered">
+//       <p>Thanks for your message</p>
+//       <Link activeClass="active" className="Contact" to="Contact" spy={true} smooth={true} duration={200} >
+//         <button className="button is-white has-text-link" onClick={()=> props.dispatch(showContactForm())}>Return to contact form</button>
+//       </Link>
+//     </div>
+//   </React.Fragment>
+// )
 
-export default connect()(ContactFormSubmitted)
+// export default connect()(ContactFormSubmitted)
